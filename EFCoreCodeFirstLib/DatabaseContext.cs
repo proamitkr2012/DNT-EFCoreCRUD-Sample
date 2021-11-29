dfc8069bcde681515527680c569a7acd81cdfe98
Amit kumar <pro.amit2012@hotmail.com>
2021-11-29 18:53:23 +0530

    Initial commit

---
+++ EFCoreCodeFirstLib/DatabaseContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreCodeFirstLib
+{
+    public class DatabaseContext :DbContext
+    {
+        public DatabaseContext()
+        {
+
+        }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) :base(options)
+        {
+
+        }
+
+        public DbSet<Department> Departments { get; set; }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("data source=LAPTOP-8VH34DE5; initial catalog=EmployeeDB_ADO;persist security info=True;user id=sa;password=developer;");
+            }
+        }
+    }
+}
